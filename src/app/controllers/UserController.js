572a44c5881cfb0f6233f60db5794ebbb09170b8
yuri <yuri.h.silva@kroton.com.br>
2020-03-25 23:54:30 -0300

    Implementa update e auth

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -57,11 +57,12 @@ class UserController {
       }
     }
 
+
     if(oldPassword && !(await user.checkPassword(oldPassword))) {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await User.update(req.body);
+    const { id, name, provider } = await user.update(req.body);
 
     return res.json({ id, name, provider });
   }
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'b9a94eecdae63faa7b768b100b377ab2',
+  expiresIn: '7d',
+}
